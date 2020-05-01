5e3c3d5b36335363ca057aa96c5567bc59540ea8
Alexsandro Silva <alexsandro.ssantos75@gmail.com>
2020-04-30 23:49:22 -0300

    Corrigindo require authService

--- src/config/routes.js
+++ src/config/routes.js
@@ -18,7 +18,7 @@ module.exports = function(app){
     const openApi = express.Router()
     app.use('/oapi', openApi) 
 
-    const AuthService = require('../api/user/AuthService')
+    const AuthService = require('../api/user/authService')
     openApi.post('/login', AuthService.login)
     openApi.post('/signup', AuthService.signup)
     openApi.post('/validateToken', AuthService.validateToken)
