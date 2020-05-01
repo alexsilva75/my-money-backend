ee62b0a5ac0e5edc9bb2f0c6a254983628857a0d
Alexsandro Silva <alexsandro.ssantos75@gmail.com>
2020-04-30 23:45:42 -0300

    Adicionando arquivos

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect(`mongodb://localhost/mymoney`)
+const url = process.env.MONGOATLAS_URI ? process.env.MONGOATLAS_URI : 'mongodb://localhost/mymoney'
+module.exports = mongoose.connect(url, {useNewUrlParser: true})
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = 
