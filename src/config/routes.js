d4768e346a1b646eb4cccf932e8e63e4016f8c4a
Alexsandro Silva <alexsandro.ssantos75@gmail.com>
2020-04-30 21:53:44 -0300

    My Money Backend Cod3r

---
+++ src/config/routes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const auth = require('./auth')
+
+module.exports = function(app){
+
+    /**
+     * Rotas protegidas
+     */
+    const protectedApi = express.Router()
+    app.use('/api', protectedApi)
+
+    protectedApi.use(auth)
+
+    const billingCycleService = require('../api/billingCycle/billingCycleService')
+    billingCycleService.register(protectedApi, '/billingCycles')
+
+    /**Rotas abertas */
+    const openApi = express.Router()
+    app.use('/oapi', openApi) 
+
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+
+
+}
---
+++ src/config/server.js
@@ -0,0 +1,28 @@
+const port = 3003
+const express = require('express')
+const bodyParser = require('body-parser')
+const queryParser = require('express-query-int')
+const cacheControl = require('express-cache-controller')
+
+const allowCors = require('./cors')
+
+const cors = require('cors')
+
+const app = express()
+
+app.options('*',cors())
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cors())
+app.use(cacheControl({noCache: true}))
+
+app.unsubscribe(queryParser()) 
+
+
+
+app.listen(port, () =>{
+    console.log(`Server listening at ${port}`)
+})
+
+module.exports = app
